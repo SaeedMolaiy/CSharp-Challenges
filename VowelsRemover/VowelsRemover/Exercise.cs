367ef5f0fe3c5415a79aa09735c3c6ec814e74d5
Saeed Molaiy <101869414+SaeedMolaiy@users.noreply.github.com>
2023-09-04 02:00:47 +0330

    #1 Challange 1 : Disemvowel Trolls

---
+++ VowelsRemover/VowelsRemover/Exercise.cs
@@ -0,0 +1,41 @@
+﻿namespace VowelsRemover
+{
+    internal class Exercise
+    {
+        private readonly char[] _chars = {
+            'e','i','o','a','u',
+              'E','I','O','A','U'
+        };
+
+        public string RemoveVowels(string value)
+        {
+            var lastSafeIndex = 0;
+
+            for (var i = 0; i < value.Length; i++)
+            {
+                for (var j = 0; j < _chars.Length; j++)
+                {
+                    if (value[i] != _chars[j])
+                    {
+                        if (i == 0)
+                        {
+                            lastSafeIndex = i;
+                        }
+                        else
+                        {
+                            lastSafeIndex = i - 1;
+                        }
+
+                        continue;
+                    }
+
+                    j = 0;
+                    value = value.Remove(i, 1);
+                    i = lastSafeIndex;
+                }
+            }
+
+            return value;
+        }
+    }
+}
---
+++ VowelsRemover/VowelsRemover/Program.cs
@@ -0,0 +1,9 @@
+﻿using VowelsRemover;
+
+const string text = "im saeed molaiy :)";
+
+var exercise = new Exercise();
+
+Console.WriteLine(exercise.RemoveVowels(text));
+
+Console.ReadKey();
